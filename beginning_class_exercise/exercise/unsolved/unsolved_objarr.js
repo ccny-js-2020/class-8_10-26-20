6a6cc5e816ad38ee195e22b01e60c88734be47ce
Jared Thomas <jaredthomas@Jareds-MacBook-Air.local>
2020-10-26 17:53:02 -0400

    pushing up lessons and exercises

---
+++ beginning_class_exercise/exercise/unsolved/unsolved_objarr.js
@@ -0,0 +1,15 @@
+/*
+
+//1
+ - change the dolphin to the whale, and make the whale larger
+
+//2
+ - when the button is clicked, add the text from the input box to the html to bold and uppercase
+
+//3
+ - create an array of objects.
+ - loop through the array of objects
+ - console.log one of the properties values for each of the objects
+ - append all of the objects and their properties to the table
+
+*/
